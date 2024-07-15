95f660ce38b1cba112c7183d39a2b43c70671ebf
Vanz1233 <149294631+Vanz1233@users.noreply.github.com>
2024-07-15 16:10:46 +0800

    Delete Assignment directory

--- Assignment/searching.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $searchQuery = $_POST['searchQuery'];
-    echo "<h1>Search Results for: " . htmlspecialchars($searchQuery) . "</h1>";
-
-}
-?>
